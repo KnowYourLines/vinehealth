6be41b0156006ced63299ec0a7d3ab740b126172
KnowYourLines <knowyourlines@outlook.com>
2021-01-24 19:58:30 +0000

    finished post endpoint, moved logic to serializer

--- project/serializers.py
+++ project/serializers.py
@@ -1,4 +1,7 @@
-from project import ma
+from marshmallow import post_load, fields
+from marshmallow.validate import OneOf
+
+from project import ma, db
 from project.models import Licence
 
 
@@ -6,3 +9,43 @@ class LicenceSchema(ma.SQLAlchemySchema):
     class Meta:
         model = Licence
         fields = ("licence",)
+
+    first_name = fields.String()
+    middle_name = fields.String()
+    last_name = fields.String()
+    date_of_birth = fields.Date()
+    gender = fields.String(validate=OneOf(["M", "F"]))
+
+    @post_load
+    def make_licence(self, data, **kwargs):
+        payload = data
+
+        last_name = payload["last_name"]
+        digit_1_5 = last_name[:6]
+        digit_1_5 = digit_1_5 + "".join("9" for _ in range(5 - len(digit_1_5)))
+
+        date_of_birth = payload["date_of_birth"]
+        gender = payload["gender"]
+        digit_6 = date_of_birth[2]
+
+        month_of_birth = int(date_of_birth.split("-")[1])
+        if gender == "F":
+            month_of_birth += 5
+        digit_7_8 = f"{month_of_birth:02}"
+        digit_9_10 = date_of_birth.split("-")[2][:2]
+        digit_11 = date_of_birth[3]
+        digit_12 = payload["first_name"][0]
+        digit_13 = payload["middle_name"][0] or "9"
+        licence_number = (
+            digit_1_5
+            + digit_6
+            + digit_7_8
+            + digit_9_10
+            + digit_11
+            + digit_12
+            + digit_13
+        )
+        new_licence = Licence(licence=licence_number)
+        db.session.add(new_licence)
+        db.session.commit()
+        return new_licence.licence
--- project/views.py
+++ project/views.py
@@ -8,27 +8,8 @@ bp = Blueprint("licence", __name__)
 
 @bp.route("/licence", methods=["POST"])
 def licence():
-    payload = request.data
-
-    last_name = payload["last_name"]
-    digit_1_5 = last_name[:6]
-    digit_1_5 = digit_1_5 + "".join("9" for _ in range(5 - len(digit_1_5)))
-
-    date_of_birth = payload["date_of_birth"]
-    gender = payload["gender"]
-    digit_6 = date_of_birth[2]
-
-    month_of_birth = int(date_of_birth.split("-")[1])
-    if gender == "F":
-        month_of_birth += 5
-    digit_7_8 = f"{month_of_birth:02}"
-    digit_9_10 = date_of_birth.split("-")[2][:2]
-    digit_11 = date_of_birth[3]
-    digit_12 = payload["first_name"][0]
-    digit_13 = payload["middle_name"][0] or "9"
-    licence_number = (
-        digit_1_5 + digit_6 + digit_7_8 + digit_9_10 + digit_11 + digit_12 + digit_13
-    )
+    result = LicenceSchema().load(request.data)
+    return Response(result)
 
 
 @bp.route("/licences", methods=["GET"])
