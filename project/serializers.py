a5c026ce1d01ef6c35ee535c25218920e9e36708
KnowYourLines <knowyourlines@outlook.com>
2021-01-24 22:03:44 +0000

    slightly more readable licence number generation

--- project/serializers.py
+++ project/serializers.py
@@ -26,16 +26,15 @@ class LicenceSchema(ma.SQLAlchemySchema):
 
     def make_licence_number(self, data):
         last_name = data.last_name
-        digit_1_5 = last_name[:6]
-        digit_1_5 = digit_1_5 + "".join("9" for _ in range(5 - len(digit_1_5)))
-
         date_of_birth = data.date_of_birth.isoformat()
         gender = data.gender
-        digit_6 = date_of_birth[2]
-
         month_of_birth = int(date_of_birth.split("-")[1])
         if gender == "F":
             month_of_birth += 5
+
+        digit_1_5 = last_name[:6]
+        digit_1_5 = digit_1_5 + "".join("9" for _ in range(5 - len(digit_1_5)))
+        digit_6 = date_of_birth[2]
         digit_7_8 = f"{month_of_birth:02}"
         digit_9_10 = date_of_birth.split("-")[2][:2]
         digit_11 = date_of_birth[3]
