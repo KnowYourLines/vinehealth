985bdf1e150465e6838dd09ce986d57c754f7de8
KnowYourLines <knowyourlines@outlook.com>
2021-01-25 02:49:13 +0000

    updated unit tests

--- project/unit_tests/test_serializers.py
+++ project/unit_tests/test_serializers.py
@@ -9,39 +9,23 @@ from project.serializers import DriverSchema
 
 
 class DriverSchemaTest(unittest.TestCase):
-    def test_extracts_licence_number_correctly(self):
+    def test_serializes_licence_number_correctly(self):
         data = {
-            "last_name": "a",
-            "first_name": "b",
-            "middle_name": "c",
-            "gender": "M",
-            "date_of_birth": datetime.date(1999, 9, 9),
+            "licence_number": "a9999909099bc",
+            "id": 2,
         }
         new_driver = Driver(**data)
         result = DriverSchema().dump(new_driver)
         assert result == "a9999909099bc"
 
-        data["gender"] = "F"
-        new_driver = Driver(**data)
-        result = DriverSchema().dump(new_driver)
-        assert result == "a9999914099bc"
-
-    def test_extracts_licence_number_correctly_for_many_drivers(self):
+    def test_serializes_licence_number_correctly_for_many_drivers(self):
         data = {
-            "last_name": "a",
-            "first_name": "b",
-            "middle_name": "c",
-            "gender": "M",
-            "date_of_birth": datetime.date(1999, 9, 9),
+            "licence_number": "a9999909099bc",
+            "id": 2,
         }
         new_driver = Driver(**data)
         result = DriverSchema(many=True).dump([new_driver, new_driver])
         assert result == ["a9999909099bc", "a9999909099bc"]
-
-        data["gender"] = "F"
-        new_driver = Driver(**data)
-        result = DriverSchema(many=True).dump([new_driver, new_driver])
-        assert result == ["a9999914099bc", "a9999914099bc"]
 
     def test_deserializes_valid_data(self):
         data = {
@@ -53,13 +37,7 @@ class DriverSchemaTest(unittest.TestCase):
         }
         data = json.dumps(data)
         input = DriverSchema().loads(data)
-        assert input == {
-            "last_name": "a",
-            "first_name": "b",
-            "middle_name": "c",
-            "gender": "M",
-            "date_of_birth": datetime.date(1999, 9, 9),
-        }
+        assert input == {"licence_number": "a9999909099bc"}
 
     def test_fails_to_deserialize_invalid_data(self):
         data = {
