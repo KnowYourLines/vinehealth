ba5c87397a42c49470d92df352ef58a0d25a5efd
KnowYourLines <knowyourlines@outlook.com>
2021-01-25 05:16:54 +0000

    expanded unit test

--- project/unit_tests/test_serializers.py
+++ project/unit_tests/test_serializers.py
@@ -26,7 +26,7 @@ class DriverSchemaTest(unittest.TestCase):
         result = DriverSchema(many=True).dump([new_driver, new_driver])
         assert result == ["a9999909099bc", "a9999909099bc"]
 
-    def test_deserializes_valid_data(self):
+    def test_deserializes_valid_data_correctly(self):
         data = {
             "last_name": "a",
             "first_name": "b",
@@ -37,6 +37,16 @@ class DriverSchemaTest(unittest.TestCase):
         data = json.dumps(data)
         input = DriverSchema().loads(data)
         assert input == {"licence_number": "a9999909099bc"}
+        data = {
+            "last_name": "aa",
+            "first_name": "basdfs",
+            "middle_name": "cfasfds",
+            "gender": "F",
+            "date_of_birth": "1999-09-09",
+        }
+        data = json.dumps(data)
+        input = DriverSchema().loads(data)
+        assert input == {"licence_number": "aa999914099bc"}
 
     def test_fails_to_deserialize_invalid_data(self):
         data = {
